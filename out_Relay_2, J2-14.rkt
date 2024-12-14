49ad033cbb65b5a0f904d5b560a664a784daa6fd
Vladislav Shcherbakov <vlad.shcherbakov@fireflyautomatix.com>
2024-12-14 09:30:51 -0700

    Add Relay 2

---
+++ out_Relay_2, J2-14.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+; out_Relay_2, J2-14.rkt
+
+(connect `PS_RAIL1 `n1-32)
+(ps-set `PS_RAIL1 24)
+
+(connect `DMM_V_HI `J2-14)
+(connect `DMM_LO `GND)
+
+(expect "Enabled auxilary power, relay K2. Expected 24V on J2-14" (eq-within `AI0 24 1))
