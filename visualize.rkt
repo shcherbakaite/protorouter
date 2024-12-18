13777bb3029adc0b790faaa954d28166b9ada241
Vladislav Shcherbakov <vlad.shcherbakov@fireflyautomatix.com>
2024-12-17 17:55:45 -0700

    update

--- visualize.rkt
+++ visualize.rkt
@@ -7,24 +7,42 @@
 
 (require "connections.rkt")
 
+
+(require "targets.rkt")
+
 (require racket/set)
 
 (define bar-width 20)
 (define last-bar  -1)
  
 (define (setup)
-  (size 640 360)  
+  (size 2200 360)  
   (color-mode 'hsb height height height)
   (background 0)
   (no-stroke))
 
-;(define (draw-x-axis )
+(define (draw-x-axis labels offset-x)
+  (when (not (null? labels))
+    (push-matrix)
+    (translate offset-x 80)
+    (rotate (radians -90))
+    (text-size 8)
+    (text (car labels) 0 0)
+    (pop-matrix)
+    (draw-x-axis (cdr labels) (+ offset-x 10)  )))
+
+(define (draw-y-axis labels offset-y)
+  (when (not (null? labels))
+    (push-matrix)
+    (translate 10 offset-y)
+    (text-size 8)
+    (text (car labels) 0 0)
+    (pop-matrix)
+    (draw-y-axis (cdr labels) (+ offset-y 10)  )))
 
 (define (draw)
   (define which-bar (quotient mouse-x bar-width))
-  (text (format "~s" (set-count connections)) 10 140)
-  (unless (= which-bar last-bar)
-    (define bar-x (* which-bar bar-width))
-    (fill mouse-y height height)
-    (rect bar-x 0 bar-width height)
-    (set! last-bar which-bar)))
+  ;(text (format "~s" (set-count connections)) 10 140)
+  (draw-x-axis (map symbol->string X-axis) 20)
+  (draw-y-axis (list "1" "2" "3" "4" "5" "6") 100 )
+  )
