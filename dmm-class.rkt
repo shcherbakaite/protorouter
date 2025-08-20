175827a7f225abd6adc18405d6b98ff71c9bb942
Vlad Shcherbakov <vlad.shchebakov@gmail.com>
2025-08-19 22:55:23 -0600

    test

--- dmm-class.rkt
+++ dmm-class.rkt
@@ -27,6 +27,23 @@
 
 (require libserialport)
 
+(displayln (serial-ports))
+
+(define-values (in out)
+  (open-serial-port  "/dev/ttyUSB0" #:baudrate 115200))
+
+(define buf (make-bytes 100))
+
+(let loop ()
+  (define n (read-bytes-avail! buf in))
+  (when (positive? n)
+    (define msg (subbytes buf 0 n))
+    ;; Print bytes in hex
+    (for ([b (in-bytes msg)])
+      (printf "~02x " b))
+    (newline))
+  (sleep 0.5)
+  (loop))
 
 
 ;; Basic DMM Class Definition
